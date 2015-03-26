bfb6ec8afc4d4043ce3c13f31642551f30bf2319
brilacerda <gabrielamota29@gmail.com>
2015-03-26 10:01:07 -0300

    Adicionando os exercicios da aula de hoje

--- 20150326.hs
+++ 20150326.hs
@@ -1,3 +1,5 @@
+--Trabalho 3
+--Questao 1
 type Hash = [(Int, Int)]
 
 get :: Hash -> Int -> Int
@@ -5,13 +7,8 @@ get ((k, v):hs) key
    |k == key = v
    |otherwise = get hs key
 
---caso nao interesse se a chave for repetida
 put :: Hash -> Int -> Int -> Hash
-put hs k v = hs ++ (k , v):[]
-
---caso nao possa chave repetida
-put2 :: Hash -> Int -> Int -> Hash
-put2 hs k v = 
+put hs k v = 
    if hasKey hs k == False
    then hs ++ (k , v):[]
    else hs
@@ -28,9 +25,29 @@ comparaConjuntos :: (Eq t) => [t] -> [t] -> String
 comparaConjuntos a b 
    |(length([x|x<-a, algum b x]) == (length a)) && (length([x|x<-b, algum a x]) == length b)  = "A igual a B"
    |length([x|x<-a, algum b x]) == 0 = "Conjuntos disjuntos"
-   |length([x|x<-a, algum b x]) == length a = "A contem B"
-   |length([x|x<-b, algum a x]) == length b = "B contem A"
+   |length([x|x<-a, algum b x]) == length b = "A contem B"
+   |length([x|x<-b, algum a x]) == length a = "B contem A"
    |length([x|x<-a, algum b x]) < length a = "A interseciona B"
    
 algum :: (Eq t) => [t] -> t -> Bool
 algum b x = length ([y|y<-b, x==y]) > 0
+
+--Exercicios
+takeN :: [t] -> Int -> [t]
+takeN [] _ = []
+takeN l 0 = []
+takeN (l:ls) n = l : takeN ls (n-1)
+
+dropN :: [t] -> Int -> [t]
+dropN [] _ = []
+dropN (l:ls) 0  = l : dropN ls 0
+dropN (l:ls) n = dropN ls (n-1)
+
+bigger10 :: Int -> Bool
+bigger10 x = x > 10
+
+takeWhileX :: (t -> Bool) -> [t] -> [t]
+takeWhileX _ [] = []
+takeWhileX f (l:ls)  
+   |f l = l : takeWhileX f ls
+   |not (f l) = takeWhileX f []
