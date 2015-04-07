58efe945ea8b55c1f145705487f13f4f22ed7685
brilacerda <gabrielamota29@gmail.com>
2015-04-07 11:38:46 -0300

    Primeiros exercicios da aula

---
+++ 20150407.hs
@@ -0,0 +1,38 @@
+data Shape = Circle Float | Rectangle Float Float
+area :: Shape -> Float
+area (Circle r) = pi*r*r
+area (Rectangle c l) = c*l
+
+type HorasAula = Int
+type Disciplina = [String]
+
+--Segunda, Terca, Quarta, Quinta, Sexta, Sabado e Domingo são construtores
+data Dia = Segunda HorasAula Disciplina| Terca HorasAula Disciplina| Quarta HorasAula Disciplina| Quinta HorasAula Disciplina| Sexta HorasAula Disciplina| Sabado | Domingo
+
+ehFDS :: Dia -> Bool
+ehFDS Sabado = True
+ehFDS Domingo = True
+ehFDS _ = False
+
+-- definicão preguiçosa
+temPLC :: Dia -> Bool
+temPLC (Terca _ _) = True
+temPLC (Quinta _ _) = True
+temPLC (_         ) = False
+
+-- definicão não preguiçosa
+plcHoje :: Dia -> Bool
+plcHoje Sabado = False
+plcHoje Domingo = False
+plcHoje (Segunda h dis) = contido dis "PLC"
+plcHoje (Terca h dis) = contido dis "PLC"
+plcHoje (Quinta h dis) = contido dis "PLC"
+plcHoje (Quarta h dis) = contido dis "PLC"
+plcHoje (Sexta h dis) = contido dis "PLC"
+
+contido :: Disciplina -> String -> Bool
+contido [] xis = False
+contido (a:as) xis
+    |a == xis = True
+    |otherwise = contido as xis
+
