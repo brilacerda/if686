db046d43e76abb119d4aef69f1c8f3ad0d2c5a19
brilacerda <gabrielamota29@gmail.com>
2015-04-09 20:01:00 -0300

    continuacao dos exercicios

--- 20150407.hs
+++ 20150407.hs
@@ -40,15 +40,31 @@ data Expr = Lit Int |
    Add Expr Expr |
    Sub Expr Expr
 
-data List t = Nil |
+data List t = Nul |
    Cons t (List t)
+   deriving (Show)
 
-data Tree t = NilT |
+data Tree t = NulT |
    Node t (Tree t) (Tree t) 
    deriving (Eq, Show)
 
+--ex. de teste usando construtor showExpr (Add (Lit 7) (Lit 6))
 showExpr :: Expr -> String
 showExpr (Lit x) = show x
 showExpr (Add a b) = showExpr a ++ " + " ++ showExpr b
 showExpr (Sub a b) = showExpr a ++ " - " ++ showExpr b
+
+--ex. teste toList (Cons 8 (Nul)) / toList (Cons 8 (Cons 4 (Cons 6 (Nul))))
+toList :: List t -> [t]
+toList (Nul) = []
+toList (Cons x ls) = (x:(toList ls))
+
+fromList :: (Show t) => [t] -> List t
+fromList [] = Nul
+fromList (l:ls) = (Cons l (fromList ls))
+
+-- teste: depth (Node 5 (Node 3 (Nult)) (Node 2 (Node 3 (Nult))))
+depth :: Tree t -> Int
+depth NulT = 0
+depth (Node c a b) = 1+ max ((depth a), (depth b))
 
